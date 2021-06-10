e7ede87a477093304322621be73826c43335589e
Jacob Short <thatmanjshort@gmail.com>
2021-06-10 13:22:28 -0700

    set up functions to generate random accounts and to check the users answer

--- main.py
+++ main.py
@@ -4,3 +4,26 @@ from ascii_art import logo, vs
 import os
 
 clear = lambda: os.system('clear')
+
+def get_random_account():
+    '''get data from random account'''
+    return random.choice(data)
+
+def format_data(account):
+    '''format account into printable format: name, desc, country'''
+    name = account['name']
+    description = ['description']
+    country = account['country']
+    return f'{name}, a {description}, from {country}'
+
+def check_answer(guess, a_followers, guess, b_followers):
+    '''Checks followers against users and
+    returns True if they got it right or 
+    false if they got it wrong.    
+     '''
+    if a_followers > b_followers:
+        return guess == 'a'
+    else:
+        return guess == 'b'
+
+
