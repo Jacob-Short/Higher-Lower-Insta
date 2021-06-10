53298b5fde79bbbc98a24cdd63d1580de118c4b7
Jacob Short <thatmanjshort@gmail.com>
2021-06-10 13:15:37 -0700

    inital commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Higher-Lower-Insta
+
+__references__
+https://www.udemy.com/course/100-days-of-code/
---
+++ main.py
@@ -0,0 +1,6 @@
+from game_data import data
+import random
+from ascii_art import logo, vs
+import os
+
+clear = lambda: os.system('clear')
